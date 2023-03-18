70e3f4f2b8d9667e9656198c5f977f7321aa2f26
Visanu <mvisanu@gmail.com>
2023-03-18 12:27:43 -0500

    Add formvalidator and signup form

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import ValidateForm from 'src/app/helpers/validateform';
 
 @Component({
   selector: 'app-login',
@@ -10,10 +12,15 @@ export class LoginComponent implements OnInit {
   type: string = "password";
   isText: boolean = false;
   eyeIcon: string = "fa-eye-slash";
+  loginForm!: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.loginForm = this.fb.group ({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    })
   }
 
   showPassword() {
@@ -21,5 +28,19 @@ export class LoginComponent implements OnInit {
     this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye-slash";
     this.isText ? this.type = "text" : this.type = "password";
   }
+
+  onSubmit() {
+    if(this.loginForm.valid) {
+      //send the obj to api
+      console.log(this.loginForm.value);
+    } else {
+      //throw the error using toaster and with required fields
+      console.log("form is not valid");
+      ValidateForm.validateAllFormFields(this.loginForm);
+      alert("Your form is invalid");
+    }
+  }
+
+
 
 }
--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import ValidateForm from 'src/app/helpers/validateform';
 
 @Component({
   selector: 'app-signup',
@@ -10,9 +12,18 @@ export class SignupComponent implements OnInit {
   type: string = "password";
   isText: boolean = false;
   eyeIcon: string = "fa-eye-slash";
-  constructor() { }
+  signUpForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.signUpForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      username: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    })
   }
 
   showPassword() {
@@ -20,5 +31,19 @@ export class SignupComponent implements OnInit {
     this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye-slash";
     this.isText ? this.type = "text" : this.type = "password";
   }
+
+  onSubmit() {
+    if(this.signUpForm.valid) {
+      //send the obj to api
+      console.log(this.signUpForm.value);
+    } else {
+      //throw the error using toaster and with required fields
+      console.log("form is not valid");
+      ValidateForm.validateAllFormFields(this.signUpForm);
+      alert("Your form is invalid");
+    }
+  }
+
+
 
 }
